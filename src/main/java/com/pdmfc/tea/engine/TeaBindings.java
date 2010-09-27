338b7467e0a4c57e1989f5dfb544648ed5694247
jfn <jorge.nunes@pdmfc.com>
2010-09-27 17:55:37 +0000

    Removed warning on List with generics. Minor code formating changes to better follow Java coding conventions.

--- src/main/java/com/pdmfc/tea/engine/TeaBindings.java
+++ src/main/java/com/pdmfc/tea/engine/TeaBindings.java
@@ -32,22 +32,34 @@ import com.pdmfc.tea.runtime.STeaRuntime;
 import com.pdmfc.tea.runtime.SObjSymbol;
 import com.pdmfc.tea.runtime.SNoSuchVarException;
 
-/**
- * This class allows Java code to access
- * (get, set, remove, list, etc...) variable bindings for Tea code.
- * It is basically a wrapper for the top level global context (com.pdmfc.tea.runtime.SContext)
+
+
+
+
+/**************************************************************************
+ * This class allows Java code to access (get, set, remove, list,
+ * etc...) variable bindings for Tea code.  It is basically a wrapper
+ * for the top level global context (com.pdmfc.tea.runtime.SContext)
  * implemented by the com.pdmfc.tea.runtime.STeaRuntime.
- * <p>Up to Tea 3.2.1, SContext is optimized for speed of execution, and does
- * not support unsetting (undefining) variables or listing 
+ *
+ * <p>Up to Tea 3.2.1, SContext is optimized for speed of execution,
+ * and does not support unsetting (undefining) variables or listing
  * variables. As such, methods related to these features throw the
  * java.lang.UnsupportedOperationException.<p>
- */
-public class TeaBindings implements Bindings {
+ *
+ **************************************************************************/
+
+public class TeaBindings
+    implements Bindings {
+
+
+
+
     /**
      * The top level runtime SContext. (Please see Tea javadoc
      * for actual details.)
      */
-    protected STeaRuntime _runtime;
+    protected STeaRuntime _runtime = null;
 
     /**
      * List of directories (or resource URLs) to be used as import
@@ -55,32 +67,63 @@ public class TeaBindings implements Bindings {
      * core configuration, but it can be manipulated using the
      * getImportDirList() method.
      */
-    protected List _importDirList = new ArrayList();
+    protected List<String> _importDirList = new ArrayList<String>();
 
-    /**
-     * _runtime will be initialized on 1st execution attempt, as we
-     * are not allowed to throw a ScriptException here.
-     * _importDirList is initialized from Tea core configuration.
-     * @see #getImportDirList()
-     */
+
+
+
+
+/**************************************************************************
+ *
+ * _runtime will be initialized on 1st execution attempt, as we
+ * are not allowed to throw a ScriptException here.
+ * _importDirList is initialized from Tea core configuration.
+ *
+ * @see #getImportDirList()
+ *
+ **************************************************************************/
+
     public TeaBindings() {
+
         _runtime = null;
 
         // import dirs
-        String importDirs = SConfigInfo.getProperty("com.pdmfc.tea.coreImportDir");
+        String importDirs =
+            SConfigInfo.getProperty("com.pdmfc.tea.coreImportDir");
+
         //URL importDirsUrl = STeaLauncher.class.getResource(importDirs);
         _importDirList.add(importDirs);
     }
 
-    /**
-     * Unsupported.
-     * @throws java.lang.UnsupportedOperationException
-     */
+
+
+
+
+/**************************************************************************
+ *
+ * Unsupported.
+ *
+ * @throws java.lang.UnsupportedOperationException
+ *
+ **************************************************************************/
+
     public void clear() {
+
         throw new UnsupportedOperationException("TeaBindings.clear() unsupported. Tea does not allow to undefine variables!");
     }
 
+
+
+
+
+/**************************************************************************
+ *
+ * 
+ *
+ **************************************************************************/
+
     public boolean containsKey(Object key) {
+
         try {
             String sKey = (String)key;
             SContext context = getMyRuntime().getToplevelContext();
@@ -91,48 +134,88 @@ public class TeaBindings implements Bindings {
         }
     }
 
-    /**
-     * Unsupported.
-     * @throws java.lang.UnsupportedOperationException
-     */
+
+
+
+
+/**************************************************************************
+ *
+ * Unsupported.
+ *
+ * @throws java.lang.UnsupportedOperationException
+ *
+ **************************************************************************/
+
     public boolean containsValue(Object value) {
+
         throw new UnsupportedOperationException("TeaBindings.containsValue() unsupported. Tea does not allow to undefine variables!");
     }
 
-    /**
-     * Unsupported.
-     * @throws java.lang.UnsupportedOperationException
-     */
+
+
+
+
+/**************************************************************************
+ *
+ * Unsupported.
+ *
+ * @throws java.lang.UnsupportedOperationException
+ *
+ **************************************************************************/
+
     public Set<Map.Entry<String,Object>> entrySet() {
+
         throw new UnsupportedOperationException("TeaBinding.entrySet(). Tea contexts do not support reflection!");
     }
 
-    /**
-     * Unsupported.
-     * @throws java.lang.UnsupportedOperationException
-     */
+
+
+
+
+/**************************************************************************
+ *
+ * Unsupported.
+ * @throws java.lang.UnsupportedOperationException
+ *
+ **************************************************************************/
+
     public boolean equals(Object o) {
+
         throw new UnsupportedOperationException("TeaBindings.equals() unsupported. Tea contexts do not support reflection!");
     }
 
-    /**
-     * The implementation of this method is limited to fteching values from global variables.
-     * This is a preliminary interpretation of the javax.script.Bindings specification, and
-     * may change in the future.
-     * <p>Example to fetch the value (a list) of the TEA_LIBRARY global variable:
-     * <code>SObjPair p = myBindings.get("TEA_LIBRARY");</code></p>
-     * <p>No auto-loading is performed. (Note also that the specification of the <code>javax.script.Bindings.get(...)</code> does not allow any
-     * exception to be thrown when calling this method).</p>
-     *
-     * @param key a String object containing the symbol name of the global variable to fetch.
-     *
-     * @return a reference to the Java object that represents the Tea value of the requested
-     * global variable. null if the variable name is not found in the top level global scope.
-     * Please read the
-     * Teadocs for the <code>tea.java</code> module for more information on the
-     * Tea/Java data type mappings.
-     */
+
+
+
+
+/**************************************************************************
+ *
+ * The implementation of this method is limited to fteching values
+ * from global variables.  This is a preliminary interpretation of
+ * the javax.script.Bindings specification, and may change in the
+ * future.
+ *
+ * <p>Example to fetch the value (a list) of the TEA_LIBRARY global
+ * variable: <code>SObjPair p =
+ * myBindings.get("TEA_LIBRARY");</code></p>
+ *
+ * <p>No auto-loading is performed. (Note also that the specification
+ * of the <code>javax.script.Bindings.get(...)</code> does not allow
+ * any exception to be thrown when calling this method).</p>
+ *
+ * @param key a String object containing the symbol name of the
+ * global variable to fetch.
+ *
+ * @return a reference to the Java object that represents the Tea
+ * value of the requested global variable. null if the variable
+ * name is not found in the top level global scope.  Please read
+ * the Teadocs for the <code>tea.java</code> module for more
+ * information on the Tea/Java data type mappings.
+ *
+ **************************************************************************/
+
     public Object get(Object key) {
+
         try {
             String sKey = (String)key;
             SContext context = getMyRuntime().getToplevelContext();
@@ -145,58 +228,98 @@ public class TeaBindings implements Bindings {
         }
     }
 
-    /**
-     * Unsupported.
-     * @throws java.lang.UnsupportedOperationException
-     */
+
+
+
+
+/**************************************************************************
+ *
+ * Unsupported.
+ *
+ * @throws java.lang.UnsupportedOperationException
+ *
+ **************************************************************************/
+
     @Override
     public int hashCode() {
+
         throw new UnsupportedOperationException("TeaBinding.entrySet() unsupported. Tea contexts do not support reflection!");
     }
 
-    /**
-     * Unsupported.
-     * @throws java.lang.UnsupportedOperationException
-     */
+
+
+
+
+/**************************************************************************
+ *
+ * Unsupported.
+ *
+ * @throws java.lang.UnsupportedOperationException
+ *
+ **************************************************************************/
+
     public boolean isEmpty() {
+
         throw new UnsupportedOperationException("TeaBindings.isEmpty() unsupported. Tea does not allow to undefine variables!");
     }
 
-    /**
-     * Unsupported.
-     * @throws java.lang.UnsupportedOperationException
-     */
+
+
+
+
+/**************************************************************************
+ *
+ * Unsupported.
+ * @throws java.lang.UnsupportedOperationException
+ *
+ **************************************************************************/
+
     public Set<String> keySet() {
+
         throw new UnsupportedOperationException("TeaBinding.keySet() unsupported. Tea contexts do not support reflection!");
     }
 
-    /**
-     * Even before calling eaScriptEngine.eval(...) methods, you are allowed to set
-     * global variables into the global scope, and they will reach the Tea global variable
-     * scope on the first eval.
-     *
-     * @param key a String object containing the symbol name of the global variable to set.
-     *
-     * @param value a Java object reference to the Tea value to be set. Please read the
-     * Teadocs for the <code>tea.java</code> module for more information on the
-     * Tea/Java data type mappings.
-     *
-     * @return a reference to the Java object that represents the Tea value of the requested
-     * global variable. null if an error ocurred. (Note also that the specification of the
-     * <code>javax.script.Bindings.put(...)</code> does not allow any
-     * exception to be thrown when calling this method).
-     *
-     */
-    public Object put(String key, Object value) {
+
+
+
+
+/**************************************************************************
+ *
+ * Even before calling eaScriptEngine.eval(...) methods, you are
+ * allowed to set global variables into the global scope, and they
+ * will reach the Tea global variable scope on the first eval.
+ *
+ * @param key a String object containing the symbol name of the
+ * global variable to set.
+ *
+ * @param value a Java object reference to the Tea value to be
+ * set. Please read the Teadocs for the <code>tea.java</code>
+ * module for more information on the Tea/Java data type mappings.
+ *
+ * @return a reference to the Java object that represents the Tea
+ * value of the requested global variable. null if an error
+ * ocurred. (Note also that the specification of the
+ * <code>javax.script.Bindings.put(...)</code> does not allow any
+ * exception to be thrown when calling this method).
+ *
+ **************************************************************************/
+
+    public Object put(String key,
+                      Object value) {
+
         SObjSymbol keySym;
+
         try {
-            String sKey = key;
+            String   sKey    = key;
             SContext context = getMyRuntime().getToplevelContext();
             keySym = SObjSymbol.addSymbol(sKey);
+
             try {
-                SObjVar v = context.getVarObject(keySym);
-                Object previous = v.get();
+                SObjVar v        = context.getVarObject(keySym);
+                Object  previous = v.get();
+
                 v.set(value);
+
                 return previous;
             } catch (SNoSuchVarException e) {
                 context.newVar(keySym, value);
@@ -207,18 +330,38 @@ public class TeaBindings implements Bindings {
         }
     }
 
-    public void putAll(Map<? extends String,? extends Object> toMerge) {
+
+
+
+
+/**************************************************************************
+ *
+ * 
+ *
+ **************************************************************************/
+
+    public void putAll(Map<? extends String,
+                       ? extends Object> toMerge) {
+
         for(Iterator i=toMerge.keySet().iterator(); i.hasNext(); ) {
             String key = (String)i.next();
             put(key, toMerge.get(key));
         }
     }
 
-    /**
-     * Unsupported.
-     * @throws java.lang.UnsupportedOperationException
-     */
+
+
+
+
+/**************************************************************************
+ *
+ * Unsupported.
+ * @throws java.lang.UnsupportedOperationException
+ *
+ **************************************************************************/
+
     public Object remove(Object key) {
+
         throw new UnsupportedOperationException("TeaBindings.remove(Object key) unsupported. Tea does not allow to undefine variables!");
         //try {
         //    String sKey = (String)key;
@@ -232,27 +375,51 @@ public class TeaBindings implements Bindings {
         //}
     }
 
-    /**
-     * Unsupported.
-     * @throws java.lang.UnsupportedOperationException
-     */
+
+
+
+
+/**************************************************************************
+ *
+ * Unsupported.
+ * @throws java.lang.UnsupportedOperationException
+ *
+ **************************************************************************/
+
     public int size() {
+
         throw new UnsupportedOperationException("TeaBindings.size() unsupported. Tea contexts do not support reflection!");
     }
 
-    /**
-     * Unsupported.
-     * @throws java.lang.UnsupportedOperationException
-     */
+
+
+
+
+/**************************************************************************
+ *
+ * Unsupported.
+ * @throws java.lang.UnsupportedOperationException
+ *
+ **************************************************************************/
+
     public Collection<Object> values() {
+
         throw new UnsupportedOperationException("TeaBindings.values() unsupported. Tea contexts do not support reflection!");
     }
 
-    /**
-     * Gets the STeaRuntime context. Initializes it for the 1st time,
-     * if needed.
-     */
-    public synchronized STeaRuntime getMyRuntime() throws ScriptException {
+
+
+
+
+/**************************************************************************
+ *
+ * Gets the STeaRuntime context. Initializes it for the 1st time,
+ * if needed.
+ *
+ **************************************************************************/
+    public synchronized STeaRuntime getMyRuntime()
+        throws ScriptException {
+
         // on the 1st tine instantiate an STeaRuntime
         if (_runtime == null) {
             //try {
@@ -270,16 +437,38 @@ public class TeaBindings implements Bindings {
         return _runtime;    
     }
 
-    /**
-     * @return _importDirList.
-     * This is the string list of directories/resource URLs 
-     * that Tea will search to import a file.
-     * At the present, _importDirList is initialized in the
-     * constructor with the Tea core dir. When the 1st script is run,
-     * the Tea interpreter will search for an init.tea file in every
-     * directory, and execute it.
-     */
-    public List getImportDirList() {
+
+
+
+
+/**************************************************************************
+ *
+ * @return _importDirList.
+ * This is the string list of directories/resource URLs 
+ * that Tea will search to import a file.
+ * At the present, _importDirList is initialized in the
+ * constructor with the Tea core dir. When the 1st script is run,
+ * the Tea interpreter will search for an init.tea file in every
+ * directory, and execute it.
+ *
+ **************************************************************************/
+
+    public List<String> getImportDirList() {
+
         return _importDirList;
     }
+
+
 }
+
+
+
+
+
+/**************************************************************************
+ *
+ * 
+ *
+ **************************************************************************/
+
+
